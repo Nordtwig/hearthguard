8afea13d34a1139664fa1c30689a69fbae88b01a
Noah <nordqvist.gaming@gmail.com>
2024-01-19 14:26:55 +0100

    Add sword controller, sprite and swing animation

--- ability/sword_ability_controller.gd
+++ ability/sword_ability_controller.gd
@@ -2,9 +2,20 @@ extends Node
 
 @export var sword_ability: PackedScene
 
+
 func _ready() -> void:
-	pass 
+	$Timer.timeout.connect(on_timer_timeout)
 
 
 func _process(delta: float) -> void:
 	pass
+
+
+func on_timer_timeout() -> void:
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player == null:
+		return
+	
+	var sword_instance = sword_ability.instantiate() as Node2D
+	player.get_parent().add_child(sword_instance)
+	sword_instance.global_position = player.global_position
