47a8e72cd9147b3fc377e1ed11a2ffd4bbe97334
Noah <nordqvist.gaming@gmail.com>
2024-01-29 22:29:14 +0100

    Add custom resource ability upgrades, upgrade screen, upgrade selection and effects, and entities and foreground sprite ordering

--- scenes/ability/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller.gd
@@ -4,9 +4,13 @@ extends Node
 @export var max_range : float = 150
 
 var damage = 5
+var base_wait_time
+
 
 func _ready() -> void:
+	base_wait_time = $Timer.wait_time
 	$Timer.timeout.connect(on_timer_timeout)
+	GameEvents.ability_upgrade_added.connect(on_ability_upgrade_added)
 
 
 func on_timer_timeout() -> void:
@@ -28,11 +32,23 @@ func on_timer_timeout() -> void:
 		return a_distance < b_distance
 	)
 
-	var sword_instance = sword_ability.instantiate() as SwordAbility 
-	player.get_parent().add_child(sword_instance)
+	var sword_instance = sword_ability.instantiate() as SwordAbility
+	var foreground_layer = get_tree().get_first_node_in_group("foreground_layer")
+	foreground_layer.add_child(sword_instance)
 	sword_instance.hitbox_component.damage = damage
 	sword_instance.global_position = enemies[0].global_position
 	sword_instance.global_position += Vector2.RIGHT.rotated(randf_range(0, TAU)) * 4
 	
 	var enemy_direction = enemies[0].global_position - sword_instance.global_position
 	sword_instance.rotation = enemy_direction.angle()
+
+
+func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary) -> void:
+	if upgrade.id != "sword_rate":
+		return
+	
+	var percent_reduction = current_upgrades["sword_rate"]["quantity"] * .5
+	$Timer.wait_time = base_wait_time * (1 - percent_reduction)
+	$Timer.start()
+
+	print($Timer.wait_time)
--- scenes/manager/experience_manager.gd
+++ scenes/manager/experience_manager.gd
@@ -1,12 +1,13 @@
 extends Node
 
 signal experience_updated(current_experience: float, target_experience: float)
+signal level_up(new_level: int)
 
 const TARGET_EXPERIENCE_GROWN = 5
 
 var current_experience = 0
 var current_level = 1
-var target_experience = 5
+var target_experience = 1
 
 
 func _ready() -> void:
@@ -21,6 +22,7 @@ func increment_experience(number: float) -> void:
         target_experience += TARGET_EXPERIENCE_GROWN
         current_experience = 0
         experience_updated.emit(current_experience,  target_experience)
+        level_up.emit(current_level)
 
 func on_experience_vial_collected(number: float) -> void:
     increment_experience(number)
