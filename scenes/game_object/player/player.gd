e17185a1e3d0a3648d95e7bb7bb8ce4b52eb4f64
Noah <nordqvist.gaming@gmail.com>
2024-01-30 10:01:04 +0100

    Add health bar to player, add end screen

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -4,21 +4,59 @@ extends CharacterBody2D
 
 const ACCELERATION_SMOOTHING = 25
 
+var number_colliding_bodies = 0
+
+@onready var damage_interval_timer = $DamageIntervalTimer
+@onready var health_component = $HealthComponent
+@onready var health_bar = $HealthBar
 
 func _ready() -> void:
-	pass
+    $CollisionArea2D.body_entered.connect(on_body_entered)
+    $CollisionArea2D.body_entered.connect(on_body_exited)
+    damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
+    health_component.health_changed.connect(on_health_changed)
+    update_health_display()
 
 
 func _process(delta: float) -> void:
-	var movement_vector = get_movement_vector()
-	var direction = movement_vector.normalized()
-	var target_velocity = direction * move_speed
+    var movement_vector = get_movement_vector()
+    var direction = movement_vector.normalized()
+    var target_velocity = direction * move_speed
 
-	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
-	move_and_slide()
+    velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
+    move_and_slide()
 
 
 func get_movement_vector() -> Vector2:
-	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	return Vector2(x_movement, y_movement) 
+    var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+    var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+    return Vector2(x_movement, y_movement) 
+
+
+func check_deal_damage() -> void:
+    if number_colliding_bodies == 0 || !damage_interval_timer.is_stopped():
+        return
+
+    health_component.damage(1)
+    damage_interval_timer.start()
+
+
+func update_health_display() -> void:
+    health_bar.value = health_component.get_health_percent()
+
+
+func on_body_entered(_other_body: Node2D) -> void:
+    number_colliding_bodies += 1
+    check_deal_damage()
+
+
+func on_body_exited(_other_body: Node2D) -> void:
+    number_colliding_bodies -= 1
+
+
+func on_damage_interval_timer_timeout() -> void:
+    check_deal_damage()
+
+
+func on_health_changed() -> void:
+    update_health_display()
