4e7209532c5b3803e0b3f810877fea50ee33ba99
Noah <nordqvist.gaming@gmail.com>
2024-02-02 16:53:50 +0100

    Add enemy animation and enemy death animation

--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -3,12 +3,17 @@ extends CharacterBody2D
 @export_range(0,200) var move_speed : float = 75
 
 @onready var health_component: HealthComponent = $HealthComponent
+@onready var visuals: Node2D = $Visuals
 
 
 func _process(_delta: float) -> void:
     var direction = get_direction_to_player()
     velocity = direction * move_speed
     move_and_slide()
+
+    var move_sign = sign(velocity.x)
+    if move_sign != 0:
+        visuals.scale = Vector2(-move_sign, 1)
 
 
 func get_direction_to_player():
