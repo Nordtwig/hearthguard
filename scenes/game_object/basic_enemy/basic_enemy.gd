8905fd02d8ffb14c4e1df731f12098329aae531e
Noah <nordqvist.gaming@gmail.com>
2024-02-03 13:28:53 +0100

    Add wizard enemy

--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -2,22 +2,14 @@ extends CharacterBody2D
 
 @export_range(0,200) var move_speed : float = 75
 
-@onready var health_component: HealthComponent = $HealthComponent
 @onready var visuals: Node2D = $Visuals
+@onready var velocity_component: VelocityComponent = $VelocityComponent
 
 
 func _process(_delta: float) -> void:
-    var direction = get_direction_to_player()
-    velocity = direction * move_speed
-    move_and_slide()
+    velocity_component.accelerate_to_player()
+    velocity_component.move(self)
 
     var move_sign = sign(velocity.x)
     if move_sign != 0:
         visuals.scale = Vector2(-move_sign, 1)
-
-
-func get_direction_to_player():
-    var player_node = get_tree().get_first_node_in_group("player") as Node2D
-    if player_node != null:
-        return (player_node.global_position - global_position).normalized()
-    return Vector2.ZERO
