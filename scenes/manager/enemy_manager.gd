b38a5d8ada5197bb997691a056ef03552f79767c
Noah <nordqvist.gaming@gmail.com>
2024-02-08 08:58:42 +0100

    Add impact particle effect to anvil ability

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -11,6 +11,7 @@ const SPAWN_RADIUS = 350
 
 var base_spawn_time = 0
 var enemy_table = WeightedTable.new()
+var number_to_spawn = 1
 
 
 func _ready() -> void:
@@ -52,22 +53,24 @@ func on_timer_timeout() -> void:
     if player == null:
         return
     
-    var enemy_scene = enemy_table.pick_item()
-    var enemy = enemy_scene.instantiate() as Node2D
+    for i in number_to_spawn:
+        var enemy_scene = enemy_table.pick_item()
+        var enemy = enemy_scene.instantiate() as Node2D
 
-    var entities_layer = get_tree().get_first_node_in_group("entities_layer")
-    entities_layer.add_child(enemy)
-    enemy.global_position = get_spawn_position()
+        var entities_layer = get_tree().get_first_node_in_group("entities_layer")
+        entities_layer.add_child(enemy)
+        enemy.global_position = get_spawn_position()
 
 
 func on_arena_difficulty_increased(arena_difficulty: int) -> void:
     var time_off = (.1 / 12) * arena_difficulty
     time_off = min(time_off, .7)
-    print(arena_difficulty)
     timer.wait_time = base_spawn_time - time_off
 
     if arena_difficulty == 6:
         enemy_table.add_item(wizard_enemy_scene, 15)
     elif arena_difficulty == 18:
         enemy_table.add_item(bat_enemy_scene, 5)
-
+    
+    if arena_difficulty % 6 == 0:
+        number_to_spawn += 1
